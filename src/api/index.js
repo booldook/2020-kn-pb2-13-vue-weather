bf73a76b2516913a4f2428079c000c1113557e03
booldook <booldook@gmail.com>
2020-11-25 15:39:43 +0900

    ...

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,7 @@ const CITY_PATH = '/json/city.json';
 const axCity = async () => {
 	try {
 		const r = await axios.get(CITY_PATH);
-		return { code: 200, data: r.data.cities } 
+		return r.data.cities 
 	}
 	catch(e) {
 		console.log(e);
