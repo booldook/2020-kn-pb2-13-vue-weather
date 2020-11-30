219c2a0f1c8e1533847e0df55d7f3e8604a4ee8a
booldook <booldook@gmail.com>
2020-11-30 11:31:13 +0900

    Weekly 시작

--- src/components/weather/WeatherWeekly.vue
+++ src/components/weather/WeatherWeekly.vue
@@ -1,57 +1,14 @@
-<template lang='pug'>
-	div.d-flex.flex-column.align-items-center.justify-content-center.text-center
-		div.city-wrap
-			h3.city {{ city }} , {{ country }}
-			h5.time {{ time }}
-		weather-icon.weather-icon(:icon='icon')
-		weather-temp(:temp='temp' :min='min' :max='max' :feel='feel')
-		weather-desc.text-primary(:main='main' :desc='desc')
-		weather-wind(:deg='deg' :speed='speed')
+<template lang="pug">
+	div WEEKLY
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import WeatherIcon from './WeatherIcon.vue';
-import WeatherTemp from './WeatherTemp.vue';
-import WeatherDesc from './WeatherDesc.vue';
-import WeatherWind from './WeatherWind.vue';
 
 export default {
-	name: 'weather-daily',
-	props: ['res'],
-	data() {
-		return { icon: '', temp: '', max: '', min: '', feel: '', main: '', desc: '', deg : '', speed: '', city: '', country: '', time: '' }
-	},
-	components: {
-		'weather-icon': WeatherIcon,
-		'weather-temp': WeatherTemp,
-		'weather-desc': WeatherDesc,
-		'weather-wind': WeatherWind,
-	},
-	watch: {
-		res: function (val) {
-			this.icon = val.icon || ''
-			this.temp = val.main ? val.main.temp : ''
-			this.max = val.main ? val.main.temp_max : ''
-			this.min = val.main ? val.main.temp_min : ''
-			this.feel = val.main ? val.main.feels_like : ''
-			this.main = val.weather ? val.weather[0].main : ''
-			this.desc = val.weather ? val.weather[0].description : ''
-			this.deg = val.wind ? val.wind.deg : ''
-			this.speed = val.wind ? val.wind.speed : ''
-			this.city = val.name ? val.name : '';
-			this.country = val.sys ? val.sys.country : '';
-			this.time = val.time ? val.time : '';
-		}
-	}
+	name: 'WeatherWeekly',
 }
 </script>
 
-<style lang='scss' scoped>
-@import "@/assets/css/variables.scss";
-.weather-daily {
-	max-width: 600px; width: 96%; margin: 3em auto 0 auto;
-	.weather-icon {margin: auto;}
-	.time {color: $greyColor; font-size: 0.875em;}
-}
+<style lang="scss" scoped>
+
 </style>
--- src/views/Weekly.vue
+++ src/views/Weekly.vue
@@ -1,10 +1,31 @@
 <template lang="pug">
-	h1 WEEKLY
+	weather-weekly
 </template>
 
 <script>
-export default {
+import WeatherWeekly from '../components/weather/WeatherWeekly.vue'
+import { mapGetters } from 'vuex'
 
+export default {
+	name: 'weekly',
+	components: {
+		'weather-weekly': WeatherWeekly,
+	},
+	created() {
+		console.log(this.$store.state.selectCity);
+		if(this.$route.params && this.$route.params.id)
+			this.$store.dispatch('ACT_WEEKLY', this.$route.params.id);
+		else if(this.$store.state.selectCity)
+			this.$store.dispatch('ACT_WEEKLY', this.$store.state.selectCity);
+	},
+	computed: {
+		...mapGetters(['GET_WEEKLY'])
+	},
+	watch: {
+		GET_WEEKLY: function(v) {
+			console.log(v);
+		}
+	}
 }
 </script>
 
