46b1c6b2fab5db1b3263989a8b1351564b3aa840
booldook <booldook@gmail.com>
2020-11-30 13:03:54 +0900

    WeatherWeeklyList 작업 시작

--- src/components/weather/WeatherWeekly.vue
+++ src/components/weather/WeatherWeekly.vue
@@ -1,10 +1,14 @@
 <template lang='pug'>
 	div.d-flex.flex-column.align-items-center.justify-content-center.text-center
 		weather-title(:city='city' :country='country' :time='time')
+		ul.list-wrap.f-flex.flex-column.align-items-start.justify-content-start
+			li.list(v-for='v in list')
+				weather-weekly-list(:data='v')
 </template>
 
 <script>
-import WeatherTitle from './WeatherTitle.vue';
+import WeatherTitle from './WeatherTitle.vue'
+import WeatherWeeklyList from './WeatherWeeklyList'
 export default {
 	name: 'WeatherWeekly',
 	props: ['res'],
@@ -13,17 +17,22 @@ export default {
 	},
 	components: {
 		'weather-title': WeatherTitle,
+		'weather-weekly-list': WeatherWeeklyList,
 	},
 	watch: {
 		res: function (val) {
-			console.log(val);
-			this.city = val.city ? val.city.name : '';
-			this.country = val.city ? val.city.country : '';
+			console.log(val)
+			this.city = val.city ? val.city.name : ''
+			this.country = val.city ? val.city.country : ''
+			this.list = val.list ? val.list : []
 		}
 	}
 }
 </script>
 
 <style lang="scss" scoped>
-
+	.list-wrap {
+		overflow-x: auto;
+		.list {border-bottom: 1px solid #ccc; padding: 0.5em;}
+	}
 </style>
---
+++ src/components/weather/WeatherWeeklyList.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+	
+</template>
+
+<script>
+import WeatherIcon from './WeatherIcon'
+import WeatherTemp from './WeatherTemp'
+import WeatherDesc from './WeatherDesc'
+import WeatherWind from './WeatherWind'
+export default {
+	name: 'WeatherWeeklyList',
+	props: ['data']
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
